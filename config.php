945a5881c406940d8dcb9ea8cc0d11a415889fc9
JakubLeonik <leonikjakub@gmail.com>
2022-01-20 22:20:45 +0100

    Add config file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+//Google
+// Google App Client Id
+define('CLIENT_ID', 'xxxxxxxxxxxxxxxxxxxx');
+// Google App Client Secret 
+define('CLIENT_SECRET', 'xxxxxxxxxxxxxxxxxxxx');
+// Google App Redirect Url 
+define('CLIENT_REDIRECT_URL', 'xxxxxxxxxxxxxxxxxxxx');
+
+//Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'simpleOnePageLogin');
+
+?>
